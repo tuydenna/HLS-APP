e78e44cac98ba561f308f24daf520c793196e3e7
tuydenna <dennaluck12@gmail.com>
2025-07-26 12:44:22 +0700

    fixed build error

--- src/app/components/ui/separator.tsx
+++ src/app/components/ui/separator.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Separator = React.forwardRef(({ className, ...props }, ref) => (
+const Separator = React.forwardRef(({ className, ...props }: any, ref) => (
     <div ref={ref} className={className + " shrink-0 bg-border h-[1px] w-full mt-2 mb-2"} {...props} />
 ));
 
