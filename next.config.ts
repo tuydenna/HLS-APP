babe6c6d79f8df2bcec94e94d35721b572122bf7
tuydenna <dennaluck12@gmail.com>
2025-08-13 14:13:29 +0700

    add file next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,41 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: process.env.NEXT_PUBLIC_STORAGE_PROTOCOL,
+                hostname: process.env.NEXT_PUBLIC_STORAGE_HOST,
+                port: process.env.NEXT_PUBLIC_STORAGE_PORT,
+                pathname: '/thumbnail/**',
+            },
+            {
+                protocol: process.env.NEXT_PUBLIC_STORAGE_PROTOCOL,
+                hostname: process.env.NEXT_PUBLIC_STORAGE_HOST,
+                port: process.env.NEXT_PUBLIC_STORAGE_PORT,
+                pathname: '/avatars/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'aninex.com',
+                port: "",
+                pathname: '/images/srvc/**',
+            },
+            {
+                protocol: process.env.NEXT_PUBLIC_STORAGE_PROTOCOL,
+                hostname: process.env.NEXT_PUBLIC_STORAGE_HOST,
+                port: process.env.NEXT_PUBLIC_STORAGE_PORT,
+                pathname: '/api/streaming/video/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.daisyui.com',
+                port: "",
+                pathname: '/images/stock/**'
+            },
+        ],
+    },
+};
+
+export default nextConfig;
