6881b6471e1fc828626db1ae079dfcb7c15cd09b
tuydenna <dennaluck12@gmail.com>
2025-09-05 12:05:15 +0700

    add see more/ see less feature to video description

--- src/app/watch/[id]/components/content/video-desciption.tsx
+++ src/app/watch/[id]/components/content/video-desciption.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import {IVideoPost} from "@interfaces/video-post";
 import {timeAgo} from "@util/helper";
+import {onDidMount} from "@lib/react-adapter";
 
 export default function VideoDescription({post}: {post: IVideoPost}) {
+
+    const descRef = useRef<HTMLPreElement>(null);
+    const [isMoreDesc, setIsMoreDesc] = useState<boolean>(false);
+
+    onDidMount(function () {
+        // Check if the content is overflowing
+        const isClamped: boolean = descRef.current!.scrollHeight > descRef.current!.clientHeight;
+
+        // If the text is clamped, show the "See more" button
+        if (isClamped) {
+            setIsMoreDesc(true);
+        }
+    })
+
+    function toggleReadMore() {
+        descRef.current!.classList.toggle("line-clamp-3");
+        setIsMoreDesc(!isMoreDesc);
+    }
+
     return (
-        <div className="bg-gray-100 rounded-sm p-2">
+        <div className="relative bg-gray-100 rounded-sm p-2 overflow-hidden">
             <span className="text-sm font-semibold">{timeAgo(post.createdAt)}</span>
-            <p className="text-muted-foreground leading-relaxed">
+            <pre ref={descRef} className={`text-muted-foreground leading-relaxed text-wrap line-clamp-3`}>
                 {post.description}
-            </p>
+            </pre>
+            <button id="toggle-read-more" onClick={toggleReadMore} className={`${isMoreDesc ? "block" : "hidden"} absolute bottom-2 right-2 z-10  pl-4 bg-gray-100 text-blue-500 font-semibold cursor-pointer`}>
+                ...see more
+            </button>
+            <button id="toggle-read-more" onClick={toggleReadMore} className={`${isMoreDesc ? "hidden" : "block"} absolute bottom-2 right-2 z-10  pl-4 bg-gray-100 text-blue-500 font-semibold cursor-pointer`}>
+                ...see less
+            </button>
         </div>
     )
 }
