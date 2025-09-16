9f303068b5e722d9bc912432abad0b01e7ca2e24
tuydenna <dennaluck12@gmail.com>
2025-09-16 16:38:05 +0700

    config middleware on register and login routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,26 @@ import {RoutesList} from "@util/routes";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    const currentUser = request.cookies.get('auth_token')?.value
+    const currentUser: string |undefined = request.cookies.get('auth_token')?.value
 
-    if (!currentUser && !request.nextUrl.pathname.startsWith(RoutesList.LOGIN)) {
-        return Response.redirect(new URL(RoutesList.LOGIN, request.url))
+    if (!currentUser) {
+        if (request.nextUrl.pathname.startsWith(RoutesList.REGISTER)) {
+            return NextResponse.next();
+        }
+        if (!request.nextUrl.pathname.startsWith(RoutesList.LOGIN)) {
+            return Response.redirect(new URL(RoutesList.LOGIN, request.url))
+        }
     }
+
+    if (currentUser && (request.nextUrl.pathname.startsWith(RoutesList.LOGIN) || request.nextUrl.pathname.startsWith(RoutesList.REGISTER))) {
+        return Response.redirect(new URL(RoutesList.HOME, request.url))
+    }
+
     return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
     // matcher:  [RoutesList.HOME, RoutesList.UPLOAD_STUDIO, RoutesList.PROFILE, "/display/:id*"],
-    matcher: ["/", "/watch/:id*", "/studio", "/search/:searchKey*"]
+    matcher: ["/", "/watch/:id*", "/studio", "/search/:searchKey*", "/auth/login", "/auth/profile", "/auth/register"],
 }
