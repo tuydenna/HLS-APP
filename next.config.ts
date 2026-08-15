bb6c561f148846f297b94f8e3d4dc5b8fdaf18a0
tuydenna <dennaluck12@gmail.com>
2026-08-15 13:12:37 +0700

    remove lint config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     reactStrictMode: false,
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     images: {
         remotePatterns: [
             {
